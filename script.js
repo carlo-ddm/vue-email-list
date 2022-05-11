16f6f2269f4b3c108d8e65eb264d349f397e98e6
carlo-ddm <carloduminuco90@gmail.com>
2022-05-11 16:02:41 +0200

    Esercizio completo

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 const app = new Vue({
   el: '#app',
   data: {
-
+    emailArray: [],
+    isLoading: true,
   },
   
   methods: {
-    
+    generaFrase(){
+      for(let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(res =>{
+          // console.log(res.data);
+          const email = res.data.response;
+          this.emailArray.push(email);
+          this.isLoading = false;
+        })
+      }
+      console.log(this.emailArray);
+    }
   },
 
   mounted() {
-    
-  },
+    this.generaFrase();
+  }
 })
